f5ac8c9d67205aff3ea14c5a96bdd5dcc4bd5703
naveenpraveen <48952772+naveenpraveen@users.noreply.github.com>
2019-06-17 22:44:18 +0530

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int n,temp,sum=0,rem;
+    printf("enter the number");
+    scanf("%d",&n);
+    temp=n;
+    while(temp!=0)
+    {
+        rem=temp%10;
+        sum=sum*10+rem;
+        temp=temp/10;
+    }
+    if(sum==n)
+    printf("yes");
+    else
+    printf("no");
+    return 0;
+}
+    
+    
